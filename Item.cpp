ea2ef8b8380f34442f0634e87073784877433404
Vihangie  Yasuththara <87426224+IT20172282@users.noreply.github.com>
2021-09-19 18:33:26 +0000

    i did tiutorial 7

--- Item.cpp
+++ Item.cpp
@@ -2,6 +2,25 @@
 #include <iostream>
 using namespace std;
 
+Item::Item()
+{
+ itemcode - 0;
+  unitprice = 0; 
+  discount = 0; 
+}
+  
+  Item::Item(int code, float price)
+  {
+   itemCode - code; 
+   unitprice - price;
+   }
+   
+    Item::~Item()
+    {
+     cout<<"Destructor called"<<endl;
+    }
+
+
 // 4. Implement Default Constructor Implementation
 // 5. Implement Overloaded Constructor Implementation
 // 6. Implement Destructor (display "Destructor Called")
